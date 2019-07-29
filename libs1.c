dfe6c303af15b2d9fc0046f068e4f926eeaf93b4
Santiago <sagudecod97@gmail.com>
2019-07-29 20:01:00 +0000

    Modified length function

--- libs1.c
+++ libs1.c
@@ -70,7 +70,9 @@ int length(char *str)
 	int i = 0;
 
 	while (str[i] != '\0')
+	{	
 		i++;
+	};
 
-	return- (i);
+	return (i);
 }
--- printf.c
+++ printf.c
@@ -4,7 +4,7 @@ int _printf(const char *format, ...)
 {
 	va_list valist;
 	int i = 0, j = 0, num;
-	char *str;
+	char *str, c;
 
 	va_start(valist, format);
 
@@ -17,28 +17,25 @@ int _printf(const char *format, ...)
 					case 'i':
 						num = va_arg(valist, int);
 						print_number(num);
-						j++;
 						i = i + 2;
 						break;
-					/*case 'c':
-						write(1,va_arg(valist, int),1);
-						j++;
+					case 'c':
+						c = va_arg(valist, int);
+						_putchar (c);
+						i = i + 2;
 						break;
 					case 's':
-						str = va_arg(valist, char *);
-						write(1,str,length(str);
-						j++;
-						break;*/
+						str = va_arg(valist, char*);
+						write(1, str, length(str));
+						i = i + 2;
+						break;
 				};
 			};
-			if (format[i] == '%')
+			if (format[i] != '\0')
 			{
-				_putchar(format[j + 1]);
-				j++;
-			}
-			_putchar(format[i]);
-			i++;
-			j++;
+				_putchar(format[i]);
+				i++;
+			};
 		};
 	write(1,"\n",1);
 	va_end(valist);
@@ -47,8 +44,12 @@ int _printf(const char *format, ...)
 int main (void)
 {
 	int numero = 4877555;
+	char c = 'c';
+	char * str = "Hola Mundo";
 
-	_printf("Hola msfef %i dsafsdfasaf fsdf", numero);
+	_printf("Hola msfef %i dsafsdfasaf fsdf %i", numero, 57878);
+	_printf("This is a char: %c", c);
+	_printf("This a fkn string: %s", str);
 
 	return (0);
 }
